25b56f2a85b82d40aa2ecf468019c9c97b63c083
Andrzej1991 <andrzej.perski@hotmail.com>
2020-07-30 16:06:30 +0200

    added footer with information and fixed header

--- script.js
+++ script.js
@@ -109,7 +109,6 @@ function changeSlide() {
 }
 
 function autoPlay() {
-  console.log(slideDots)
   if (autoSlide == slideDots - 1) {
     autoSlide = 0;
   } else {
@@ -121,3 +120,27 @@ function autoPlay() {
 let timer = setInterval(autoPlay, 5000);
 
 window.onload = load();
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+};
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a");
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+});
